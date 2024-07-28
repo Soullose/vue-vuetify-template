1cda011b53efd08c85c5b215ad31f6ebd0848b0e
WangSF <soullose@qq.com>
2024-07-28 22:13:17 +0800

    setting prettier configuration

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,7 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
@@ -41,10 +37,10 @@
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
