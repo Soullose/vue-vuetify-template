e4e11dd4542c0201b5201eeed7c3fe6f04b38f8d
WangSF <soullose@qq.com>
2024-07-29 22:53:38 +0800

    添加ENV

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
+const port = process.env.PORT;
+
 module.exports = defineConfig({
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  // 生产环境是否生成 sourceMap 文件,方便调试
+  productionSourceMap: true,
+  devServer: {
+    port: port,
+    proxy: {}
+  }
 });
