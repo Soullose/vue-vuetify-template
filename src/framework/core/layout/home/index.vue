b5a08dc128e7332423dbc0935db394c57f82b41a
WangSF <soullose@qq.com>
2025-03-09 14:43:49 +0800

    style(layout): 优化首页布局和样式
    - 移除 vuetify 名称图片，替换为文本标题
    - 调整 app-bar 样式，移除颜色属性
    - 更新 Latest Release 按钮样式，添加颜色属性

--- src/framework/core/layout/home/index.vue
+++ src/framework/core/layout/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app elevation="1">
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -10,20 +10,12 @@
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-toolbar-title class="text-h4">Vuetify</v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text color="black">
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
