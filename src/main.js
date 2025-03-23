c6fd18af42b2c6323d8c5cad8219d16dccf56db3
WangSF <soullose@qq.com>
2025-03-23 10:20:55 +0800

    refactor: 删除未使用的代码
    - 移除了 App.vue 中的整个模板结构和脚本部分
    - 删除了 main.js 中的 Vue 相关代码和导入语句
    - 简化了 main.js 中的 app.run 调用

--- src/main.js
+++ src/main.js
@@ -1,19 +1,4 @@
 import app from './framework/core';
-// import Vue from 'vue';
-// import App from './App.vue';
-// import vuetify from './plugins/vuetify';
-// import router from './router';
-// import store from './store';
-
-// Vue.config.productionTip = false;
-
-// new Vue({
-//   router,
-//   store,
-//   vuetify,
-//   render: (h) => h(App)
-// }).$mount('#app');
-
 app.run({
   startUrl: '/'
 });
