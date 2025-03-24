571039160779a84597541c83edeba5a55bf74ea7
WangSF <soullose@qq.com>
2025-03-24 15:33:28 +0800

    feat(framework): 集成 Pinia 状态管理库
    - 在 package.json 和 package-lock.json 中添加 Pinia 依赖
    - 在框架核心中初始化并使用 Pinia
    - 添加测试 store 文件以验证 Pinia 集成

--- src/framework/core/index.js
+++ src/framework/core/index.js
@@ -1,3 +1,4 @@
+import { createPinia, PiniaVuePlugin } from 'pinia';
 import 'uno.css';
 import Vue from 'vue';
 import '../components';
@@ -5,14 +6,17 @@ import App from './App.vue';
 import { vuetify } from './plugins';
 import router from './router';
 import store from './store';
+Vue.use(PiniaVuePlugin);
 Vue.config.productionTip = false;
 
+const pinia = createPinia();
 export default {
   run(options) {
     options;
     console.log('run:', options);
     return new Vue({
       router,
+      pinia,
       store,
       vuetify,
       render: (h) => h(App)
