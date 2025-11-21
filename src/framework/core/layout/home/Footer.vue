5b35799668657194739dd16ca5ad70e53cc7be06
WangSF <soullose@qq.com>
2025-11-21 09:24:22 +0800

    Reapply "refactor(layout): 重构首页布局"
    This reverts commit 8db0719124b99597d7908bd647a53180c07c7689.

---
+++ src/framework/core/layout/home/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-footer inset app color="white" elevation="12" style="height: 48px">
+    <v-spacer />
+    <span class="text-body-2 mr-2">版权所有</span>
+    <span class="text-body-2">Vuetify LLC</span>
+  </v-footer>
+</template>
+<script></script>
