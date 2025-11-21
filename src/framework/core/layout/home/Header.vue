82dc8fcbf6c2b351b600fdaf5ea578fb0499af17
WangSF <soullose@qq.com>
2025-11-21 09:22:04 +0800

    Revert "refactor(framework): 优化消息通知按钮的代码结构"
    This reverts commit b5aec8567ea0841af244fa5823e2c682fc63b1d9.

--- src/framework/core/layout/home/Header.vue
+++ src/framework/core/layout/home/Header.vue
@@ -21,8 +21,8 @@
     </v-tooltip>
 
     <v-menu offset-y left min-width="260" transition="slide-y-transition">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn icon v-on="on" v-bind="attrs">
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
           <v-badge color="red" overlap dot bordered :content="messages.length">
             <v-icon>mdi-email-outline</v-icon>
           </v-badge>
@@ -38,6 +38,7 @@
         </v-list>
       </v-card>
     </v-menu>
+
     <v-menu offset-y left min-width="260" transition="slide-y-transition">
       <template v-slot:activator="{ on }">
         <v-btn icon v-on="on">
