acb59a89e2f99cee81a3470894d0bde7bb327b05
WangSF <soullose@qq.com>
2025-11-21 09:24:32 +0800

    Reapply "refactor([...all].vue): 优化 404 页面布局和样式"
    This reverts commit eee6020c4148040805f8726db0423a7fe83bf868.

--- src/views/[...all].vue
+++ src/views/[...all].vue
@@ -5,24 +5,11 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="d-flex align-center flex-column" color="white">
-    <div style="height: 200px"></div>
-    <div class="d-flex align-center">
-      <!-- <v-icon class="logo mb-4">mdi-alert-circle</v-icon> -->
-      <p class="text-h2 transition-swing">404 页面不存在</p>
+  <div h-full text-center flex select-none>
+    <div ma>
+      <div text-5xl color-red-700 fw100>404页面不存在</div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.wrapper {
-  position: relative;
-  top: calc(50vh - 250px);
-  text-align: center;
-  opacity: 0.6;
-}
-.logo {
-  font-size: 12em;
-  opacity: 1;
-}
-</style>
+<style lang="scss" scoped></style>
