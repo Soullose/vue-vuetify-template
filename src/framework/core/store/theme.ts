47293c31745beb233776aeaeb71e8cef85d61310
WangSF <soullose@qq.com>
2025-11-21 09:24:06 +0800

    Reapply "feat(theme): 添加侧边栏折叠功能并初始化主题"
    This reverts commit 044c4ace0425ae8c6b47dab611f5d1386a03a235.

--- src/framework/core/store/theme.ts
+++ src/framework/core/store/theme.ts
@@ -3,17 +3,26 @@
  */
 interface ThemeState {
   theme: String;
+  asideMenuFolded: Boolean;
 }
 
 export const theme = defineStore('theme', {
   state: (): ThemeState => {
     return {
-      theme: 'light'
+      theme: 'light',
+      asideMenuFolded: false
     };
   },
   actions: {
+    setAsideMenuFolded(asideMenuFolded: Boolean) {
+      this.asideMenuFolded = asideMenuFolded;
+    },
     setTheme(theme: String) {
       this.theme = theme;
     }
+  },
+  getters: {
+    getAsideMenuFolded: (state) => state.asideMenuFolded,
+    getTheme: (state) => state.theme
   }
 });
