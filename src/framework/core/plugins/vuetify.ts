7636fdc5430449dd8081c3c1115d562c6bae620f
WangSF <soullose@qq.com>
2025-11-21 09:27:00 +0800

    Revert "style(vuetify): 调整样式和布局"
    This reverts commit 68bed3e245eebffbeb065a8ebd7df76ee6123b29.

--- src/framework/core/plugins/vuetify.ts
+++ src/framework/core/plugins/vuetify.ts
@@ -31,15 +31,6 @@ const theme = {
   info: '#00CAE3'
 };
 
-const light = {
-  primary: '#000',
-  secondary: '#FFD65E',
-  accent: '#EDAFB8',
-  error: '#FF5252',
-  info: '#2196F3',
-  success: '#4CAF50',
-  warning: '#FFC107'
-};
 export const install: InstallPlugin = (vue) => {
   vue.use(Vuetify, {
     directives: {
@@ -56,16 +47,8 @@ export const install: InstallPlugin = (vue) => {
     theme: {
       dark: useDark().value,
       themes: {
-        light: {
-          primary: '#1976D2',
-          secondary: '#424242',
-          accent: '#82B1FF',
-          error: '#FF5252',
-          info: '#2196F3',
-          success: '#4CAF50',
-          warning: '#FFC107',
-          anchor: '#8c9eff'
-        }
+        dark: theme,
+        light: theme
       },
       options: {
         themeCache: {
