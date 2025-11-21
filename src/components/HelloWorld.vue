28fcec8d1f5839090fe47b4c54757185ece758ce
WangSF <soullose@qq.com>
2025-11-21 09:25:15 +0800

    Revert "init"
    This reverts commit 5518caf13fe24d2308b5ffbb8c7ee31089d9e548.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -51,7 +51,6 @@
 
 <script>
 export default {
-<<<<<<< HEAD
   name: 'HelloWorld',
 
   data: () => ({
@@ -108,29 +107,3 @@ export default {
   })
 };
 </script>
-=======
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
->>>>>>> df6989c (init)
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-<<<<<<< HEAD
     path: '/',
     name: 'home',
     component: HomeView
@@ -26,25 +25,6 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-=======
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
-
-const router = new VueRouter({
-  routes,
->>>>>>> df6989c (init)
 });
 
 export default router;
