945fad84d16cbd9d39e0864e9e683c48e92ff97f
WangSF <soullose@qq.com>
2025-11-21 09:23:06 +0800

    Revert "refactor(framework): 移除旧版grid组件并更新核心配置"
    This reverts commit 564d1c0b431975384ac4df9576a2eddaa7c2eaea.

--- src/framework/components/index.js
+++ src/framework/components/index.js
@@ -1,19 +1,21 @@
-// import { XGrid } from './grid';
-// const components = { XGrid };
+import _ from 'lodash';
+import Vue from 'vue';
+import { XGrid } from './grid';
+const components = { XGrid };
 
-// var installed = false;
-// function install(Vue) {
-//   if (!installed) {
-//     _.each(components, (component, name) => {
-//       Vue.component(name, component);
-//     });
+var installed = false;
+function install(Vue) {
+  if (!installed) {
+    _.each(components, (component, name) => {
+      Vue.component(name, component);
+    });
 
-//     installed = true;
-//   }
-// }
+    installed = true;
+  }
+}
 
-// install(Vue);
+install(Vue);
 
 export default {
-  // ...components
+  ...components
 };
